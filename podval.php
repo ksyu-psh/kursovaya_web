120dbf664aefc27252c5ba93d6367a9a613f598c
ksyu-psh <129156953+ksyu-psh@users.noreply.github.com>
2023-10-19 17:59:23 +0300

    Add files via upload

---
+++ podval.php
@@ -0,0 +1,15 @@
+<div class="container">
+    <footer class="d-flex flex-wrap justify-content-between align-items-center py-2 border-top">
+        <p class="col-md-2 mb-0 text-muted">&copy; 2022 OK.SɅN</p>
+        <a href="/" class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
+            <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"/></svg>
+        </a>
+                        
+        <ul class="nav col-md-5 justify-content-end">
+            <li class="nav-item"><a href="главная.html" class="nav-link px-2 text-muted">Главная</a></li>
+            <li class="nav-item"><a href="букеты.html" class="nav-link px-2 text-muted">Букеты</a></li>
+            <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Контакты</a></li>
+            <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Служебная страница</a></li>
+        </ul>
+    </footer>
+</div>
